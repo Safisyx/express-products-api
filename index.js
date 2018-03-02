1450b8a6c4f5fe7c037c2d1ac5a4589fab782568
Safisyx <safisyx@gmail.com>
2018-03-02 11:05:45 +0100

    Extend products api

--- index.js
+++ index.js
@@ -75,3 +75,65 @@ app.get('/products/:id', (req, res) => {
       res.json({message: 'Oops'})
     })
 })
+
+app.post('/products', (req, res) => {
+  const product = req.body
+  console.log(product)
+
+  // insert the new data into our database
+  Product.create(product).then(entity => {
+
+    // send back the 201 Created status and the entity
+    res.status(201).send(entity)
+  })
+
+})
+
+
+app.put('/products/:id', (req, res) => {
+  const productId = Number(req.params.id)
+  const updates = req.body
+
+  // find the product in the DB
+  Product.findById(req.params.id)
+    .then(entity => {
+      // change the product and store in DB
+      return entity.update(updates)
+    })
+    .then(final => {
+      // respond with the changed product and status code 200 OK
+      res.send(final)
+    })
+    .catch(error => {
+      res.status(500).send({
+        message: `Something went wrong`,
+        error
+      })
+    })
+
+})
+
+
+app.delete('/products/:id', (req, res) => {
+  const productId = Number(req.params.id)
+  Product.findById(req.params.id)
+  .then(entity => {
+    // change the product and store in DB
+    return entity.destroy()
+  })
+  .then(_ => {
+    // respond with the changed product and status code 200 OK
+    res.staus(200).send({
+      message: 'The product was deleted succesfully'
+    })
+  })
+  .catch(error => {
+    res.status(500).send({
+      message: `Something went wrong`,
+      error
+    })
+  })
+  // delete the product from the DB
+  // respond with 200 OK and a message
+
+})
